451aa9f938691af70242666ada12e713e391681b
AllanSMith <allan@gmail.com>
2022-12-19 17:01:41 -0300

    Remoção de ReportWebVitals

--- alhama-gallery/src/index.js
+++ alhama-gallery/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,6 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-reportWebVitals();

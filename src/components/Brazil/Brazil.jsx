57783aab9e092471e015b0eee49e37605e51398d
AllanSmithll <allanammancio2004@gmail.com>
2024-09-28 14:26:31 -0300

    Aplicação adaptada para Firestore

--- src/components/Brazil/Brazil.jsx
+++ src/components/Brazil/Brazil.jsx
@@ -5,16 +5,27 @@ import { db } from "../../firestore.config";
 import "../../styles/MainContent.css";
 
 const Brazil = () => {
-  const [landscapes, setlandscapes] = useState([]);
+  const [landscapes, setLandscapes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchlandscapes = async () => {
-      const q = query(collection(db, "landscapes"), where("country", "==", "Brazil"));
-      const querySnapshot = await getDocs(q);
-      const fetchedlandscapes = querySnapshot.docs.map((doc) => doc.data());
-      setlandscapes(fetchedlandscapes);
+    const fetchLandscapes = async () => {
+      try {
+        const q = query(
+          collection(db, "landscapes"),
+          where("country", "==", "Brasil")
+        );
+        const querySnapshot = await getDocs(q);
+        const fetchedLandscapes = querySnapshot.docs.map((doc) => doc.data());
+        setLandscapes(fetchedLandscapes);
+      } catch (error) {
+        console.error("Erro ao buscar os pontos turísticos:", error);
+      } finally {
+        setLoading(false); // Para parar o carregamento após a busca
+      }
     };
-    fetchlandscapes();
+
+    fetchLandscapes();
   }, []);
 
   return (
@@ -22,24 +33,27 @@ const Brazil = () => {
       <div className="title-home container">
         <h2>Pontos turísticos brasileiros</h2>
       </div>
-      <div className="gallery-landscapes">
-        {landscapes.map((p, index) => (
-          <>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : landscapes.length === 0 ? (
+        <p>Não foram encontrados pontos turísticos no Brasil.</p>
+      ) : (
+        <div className="gallery-landscapes">
+          {landscapes.map((p, index) => (
             <Card key={index}>
-              <Card.Img variant="top" src={p.url} />
+              <Card.Img variant="top" src={p.url} alt={p.title} />
               <Card.Body>
                 <Card.Title>{p.title}</Card.Title>
                 <Card.Text>
                   <p><strong>Continente</strong>: {p.continent}</p>
                   <p><strong>País</strong>: {p.country}</p>
-                  <p><strong>Cidade</strong>:  {p.city}</p>
+                  <p><strong>Cidade</strong>: {p.city}</p>
                 </Card.Text>
               </Card.Body>
             </Card>
-            <br />
-          </>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
